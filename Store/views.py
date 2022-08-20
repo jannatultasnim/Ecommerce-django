a42bb8786d5cc203579d38ab14fd8c8629849a48
jannat <jannatultasnim786@gmail.com>
2022-08-20 23:40:23 +0600

    pagination and search functionality added

--- Store/views.py
+++ Store/views.py
@@ -1,10 +1,10 @@
-
-from itertools import product
 from django.shortcuts import render,get_object_or_404
 from . models import Product
 from category.models import Category
 from cart.models import CartItem
 from cart.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 
 # Create your views here.
 
@@ -14,12 +14,18 @@ def store(request,category_slug=None):
     if category_slug is not None:
         categories = get_object_or_404(Category,slug=category_slug)
         products = Product.objects.filter(category=categories,is_available=True)
+        paginator= Paginator(products,1)
+        page_number = request.GET.get('page')
+        page_products = paginator.get_page(page_number)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator= Paginator(products,3)
+        page = request.GET.get('page')
+        page_products = paginator.get_page(page)
         product_count = products.count()
     context = {
-        'product': products,
+        'product':page_products,
         'product_count': product_count,
     }
     return render(request,'store/store.html',context)
@@ -36,3 +42,13 @@ def product_detail(request,category_slug,product_slug):
         'in_cart': in_cart,}
     
     return render(request,'store/product_detail.html',context)
+
+def Search(request):
+    if 'keyword' in request.GET:
+        value= request.GET['keyword']
+        if value:
+            products = Product.objects.order_by('-created_date').filter(description__icontains=value)
+    context={
+        'product': products,
+        }
+    return render(request,'store/store.html',context)
--- ecommerceweb/settings.py
+++ ecommerceweb/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.conetxt-processors.menu_links',
+                'cart.context-processors.counter',
             ],
         },
     },
